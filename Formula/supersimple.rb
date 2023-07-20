53acb585a1dfdc25930d9b44eaf02c079e453807
Priit Haamer <priit@haamer.ee>
2023-07-20 16:19:02 +0300

    chore: upgrade to 1.3.5

--- Formula/supersimple.rb
+++ Formula/supersimple.rb
@@ -1,19 +1,19 @@
 class Supersimple < Formula
   desc "CLI tool for Supersimple configuration management"
   homepage "https://github.com/gosupersimple/supersimple-cli"
-  version "1.3.4"
+  version "1.3.5"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://storage.googleapis.com/cdn.supersimple.cloud/supersimple-cli/v#{version}/supersimple-arm64-apple-darwin", :using => :nounzip
-      sha256 "f5f905749b50964a132873cfaa990ab6e286e1f49122e4967d58a5b50a8e4765"
+      sha256 "1be01cd28304fae8650f353ffe3699d7d553791dbefa3660aadd182b79bc46a9"
 
       def install
         bin.install "cdn.supersimple.cloud" => "supersimple"
       end
     else
       url "https://storage.googleapis.com/cdn.supersimple.cloud/supersimple-cli/v#{version}/supersimple-x86_64-apple-darwin"
-      sha256 "0fc79131e65fbbb06b12431ada0cb631843a914f92e73c80ec235e7c16720b24"
+      sha256 "5dbe7d0047ac410874ed95d9347f32f23b8279bde5a90a5882d4255bbafa359e"
 
       def install
         bin.install "cdn.supersimple.cloud" => "supersimple"
@@ -23,7 +23,7 @@ class Supersimple < Formula
 
   on_linux do
     url "https://github.com/gosupersimple/supersimple-cli/releases/download/v#{version}/supersimple-x86_64-linux-gnu"
-    sha256 "d7b959cd9ff1e92fce10704261da92256f44dfe82a00318e570c87c5b8ed31e0"
+    sha256 "047028268b7be16d66e196166a271c60738d1175097534cf0c8eb53b6d4b23a7"
 
     def install
       bin.install "cdn.supersimple.cloud" => "supersimple"
