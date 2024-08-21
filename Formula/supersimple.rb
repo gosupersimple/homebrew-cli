8f44f735abcb02c4b5f1bb9bea183b5a2eae6315
priithaamer <priithaamer@users.noreply.github.com>
2024-08-21 14:09:18 +0000

    Update Formula to 2.7.3

--- Formula/supersimple.rb
+++ Formula/supersimple.rb
@@ -2,19 +2,19 @@
 class Supersimple < Formula
   desc "CLI tool for Supersimple configuration management"
   homepage "https://github.com/gosupersimple/supersimple-cli"
-  version "2.7.2"
+  version "2.7.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://storage.googleapis.com/cdn.supersimple.cloud/supersimple-cli/releases/v2.7.2/supersimple-arm64-apple-darwin", :using => :nounzip
-      sha256 "6874c6772585760f7de9205e2214e8740c78dde99a15599cc856b0e70d054454"
+      url "https://storage.googleapis.com/cdn.supersimple.cloud/supersimple-cli/releases/v2.7.3/supersimple-arm64-apple-darwin", :using => :nounzip
+      sha256 "14a9d095696719e574d2467ee021bcc95083dbf8d6f60ca641b0f137b5ee1c7b"
 
       def install
         bin.install "cdn.supersimple.cloud" => "supersimple"
       end
     else
-      url "https://storage.googleapis.com/cdn.supersimple.cloud/supersimple-cli/releases/v2.7.2/supersimple-x86_64-apple-darwin", :using => :nounzip
-      sha256 "f799c7a8243527e4a5d450d82202d303b0a9a97e4335549c82ee830069ab887c"
+      url "https://storage.googleapis.com/cdn.supersimple.cloud/supersimple-cli/releases/v2.7.3/supersimple-x86_64-apple-darwin", :using => :nounzip
+      sha256 "bac8c7fb0447cfaca623cfc1624c9f326c4b4ef8ef49fc5cb37e3b37e0db68d8"
 
       def install
         bin.install "cdn.supersimple.cloud" => "supersimple"
@@ -23,8 +23,8 @@ class Supersimple < Formula
   end
 
   on_linux do
-    url "https://storage.googleapis.com/cdn.supersimple.cloud/supersimple-cli/releases/v2.7.2/supersimple-x86_64-linux-gnu", :using => :nounzip
-    sha256 "fc91079eb88e45824548858edcb28455453d3f9886a61e1ba2163ade05b71ad7"
+    url "https://storage.googleapis.com/cdn.supersimple.cloud/supersimple-cli/releases/v2.7.3/supersimple-x86_64-linux-gnu", :using => :nounzip
+    sha256 "0888f3d9be941cd2fe57d0b1c7a0d7a78a483aa13f8c279d0351d11b70216708"
 
     def install
       bin.install "cdn.supersimple.cloud" => "supersimple"
