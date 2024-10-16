85a3ce21cfe58c8acb7bec330efbb881657b91d7
priithaamer <priithaamer@users.noreply.github.com>
2024-10-16 07:53:47 +0000

    Update Formula to 2.8.0

--- Formula/supersimple.rb
+++ Formula/supersimple.rb
@@ -2,19 +2,19 @@
 class Supersimple < Formula
   desc "CLI tool for Supersimple configuration management"
   homepage "https://github.com/gosupersimple/supersimple-cli"
-  version "2.7.10"
+  version "2.8.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://assets.supersimple.io/supersimple-cli/releases/v2.7.10/supersimple-arm64-apple-darwin", :using => :nounzip
-      sha256 "ba3159cf36a0f3e0e48dbb0b8de77a836c19d1930bee57741791921b7661993d"
+      url "https://assets.supersimple.io/supersimple-cli/releases/v2.8.0/supersimple-arm64-apple-darwin", :using => :nounzip
+      sha256 "9b47e3e3198dae1448b951d1ef58ced4dd94d07eb75c97ba2387e3a055207ad4"
 
       def install
         bin.install "supersimple-arm64-apple-darwin" => "supersimple"
       end
     else
-      url "https://assets.supersimple.io/supersimple-cli/releases/v2.7.10/supersimple-x86_64-apple-darwin", :using => :nounzip
-      sha256 "06ff2bc37000865d7a8a3deec863f996ecd512a6201b0b0c5e56df891782b437"
+      url "https://assets.supersimple.io/supersimple-cli/releases/v2.8.0/supersimple-x86_64-apple-darwin", :using => :nounzip
+      sha256 "70b783145a43de6dad5c9c445e118d80b2201fb9af8c44416329ef00dadae7d1"
 
       def install
         bin.install "supersimple-x86_64-apple-darwin" => "supersimple"
@@ -23,8 +23,8 @@ class Supersimple < Formula
   end
 
   on_linux do
-    url "https://assets.supersimple.io/supersimple-cli/releases/v2.7.10/supersimple-x86_64-linux-gnu", :using => :nounzip
-    sha256 "f61ba8c5798350aac4b5a75da4eb426c16f8a000a55dd974416f024d29d1f046"
+    url "https://assets.supersimple.io/supersimple-cli/releases/v2.8.0/supersimple-x86_64-linux-gnu", :using => :nounzip
+    sha256 "1275e58c9c2ec0548b4affccb7af24db89ac97d1fa0cdcb458c83d92926fb584"
 
     def install
       bin.install "supersimple-x86_64-linux-gnu" => "supersimple"
