335840d4156e63f62b621537c7ca87256521283b
priithaamer <priithaamer@users.noreply.github.com>
2024-11-06 18:51:36 +0000

    Update Formula to 2.9.1

--- Formula/supersimple.rb
+++ Formula/supersimple.rb
@@ -2,19 +2,19 @@
 class Supersimple < Formula
   desc "CLI tool for Supersimple configuration management"
   homepage "https://github.com/gosupersimple/supersimple-cli"
-  version "2.9.0"
+  version "2.9.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://assets.supersimple.io/supersimple-cli/releases/v2.9.0/supersimple-arm64-apple-darwin", :using => :nounzip
-      sha256 "457af47734b6ad645b1d7b56dfd6d9ba976c182aee36fa4602306e4541ba5c34"
+      url "https://assets.supersimple.io/supersimple-cli/releases/v2.9.1/supersimple-arm64-apple-darwin", :using => :nounzip
+      sha256 "be21ec9cd4364e0ed5bb82d7a060e59abce67c9e06e5500e3a5a847d43783ca3"
 
       def install
         bin.install "supersimple-arm64-apple-darwin" => "supersimple"
       end
     else
-      url "https://assets.supersimple.io/supersimple-cli/releases/v2.9.0/supersimple-x86_64-apple-darwin", :using => :nounzip
-      sha256 "ed47c0b464a5a6efd81620480ee0dbaca36ddeb5dd4ca69eef585b296d8af2e7"
+      url "https://assets.supersimple.io/supersimple-cli/releases/v2.9.1/supersimple-x86_64-apple-darwin", :using => :nounzip
+      sha256 "4b494014452c79364d059ca4cf9362aa356014879eaa2c7750e5d108d33bc530"
 
       def install
         bin.install "supersimple-x86_64-apple-darwin" => "supersimple"
@@ -23,8 +23,8 @@ class Supersimple < Formula
   end
 
   on_linux do
-    url "https://assets.supersimple.io/supersimple-cli/releases/v2.9.0/supersimple-x86_64-linux-gnu", :using => :nounzip
-    sha256 "6356b4ac6a1bee632081976d0310735be7dc532f7fba8681446c75b35e8511d1"
+    url "https://assets.supersimple.io/supersimple-cli/releases/v2.9.1/supersimple-x86_64-linux-gnu", :using => :nounzip
+    sha256 "b41ca9e928294e52e097ed593d5f3ea58c33c3c7fdf7bf729269ef260e60c823"
 
     def install
       bin.install "supersimple-x86_64-linux-gnu" => "supersimple"
