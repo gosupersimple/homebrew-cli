11560db8a07c2893217aad115c9ec4120c01b104
priithaamer <priithaamer@users.noreply.github.com>
2026-04-27 11:11:12 +0000

    Update Formula to 2.19.6

--- Formula/supersimple.rb
+++ Formula/supersimple.rb
@@ -2,19 +2,19 @@
 class Supersimple < Formula
   desc "CLI tool for Supersimple configuration management"
   homepage "https://github.com/gosupersimple/supersimple-cli"
-  version "2.19.5"
+  version "2.19.6"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://assets.supersimple.io/supersimple-cli/releases/v2.19.5/supersimple-arm64-apple-darwin", :using => :nounzip
-      sha256 "858aa3050fc4fb01176681df0c93d5894a1e37ff0ef4a4b4268ab1fe62ead52c"
+      url "https://assets.supersimple.io/supersimple-cli/releases/v2.19.6/supersimple-arm64-apple-darwin", :using => :nounzip
+      sha256 "1a5656836aa6357e51b8b067f178270a0258882d271225e2a6eb38868468d065"
 
       def install
         bin.install "supersimple-arm64-apple-darwin" => "supersimple"
       end
     else
-      url "https://assets.supersimple.io/supersimple-cli/releases/v2.19.5/supersimple-x86_64-apple-darwin", :using => :nounzip
-      sha256 "feec6db9c9abac1a046cd9c510542c487c4ae273493f524493df6a71d755ab01"
+      url "https://assets.supersimple.io/supersimple-cli/releases/v2.19.6/supersimple-x86_64-apple-darwin", :using => :nounzip
+      sha256 "ec3c3e0fbc603017adcec35587027252e8b6830dd70fdb1aa4ab2470f068e706"
 
       def install
         bin.install "supersimple-x86_64-apple-darwin" => "supersimple"
@@ -23,8 +23,8 @@ class Supersimple < Formula
   end
 
   on_linux do
-    url "https://assets.supersimple.io/supersimple-cli/releases/v2.19.5/supersimple-x86_64-linux-gnu", :using => :nounzip
-    sha256 "ace1d8ae7d00072426f08e5b0aa6d40735c8d333c4438f678d4b1597512a6143"
+    url "https://assets.supersimple.io/supersimple-cli/releases/v2.19.6/supersimple-x86_64-linux-gnu", :using => :nounzip
+    sha256 "98186baeddfa5ef4661ab7c063816323cfefb5f3d6e5e744bbbe896a8d451225"
 
     def install
       bin.install "supersimple-x86_64-linux-gnu" => "supersimple"
